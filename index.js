fded114e84a7a3967e6ecc16fb4e1a42cf24781a
Fabio Cortés <fcor14@hotmail.com>
2021-07-23 11:56:52 +0200

    Added atoms and testing scale/translation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { VRButton } from "./webxr/VRButton.js";
 import { XRControllerModelFactory } from "./webxr/XRControllerModelFactory.js";
 import { XRHandModelFactory } from "./webxr/XRHandModelFactory.js";
 import cannonDebugger from "./libs/cannon-es-debugger.js";
+import { pdb } from "./utils.js";
 
 let container;
 let camera, scene, renderer, world;
@@ -13,24 +14,49 @@ let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 let myDebugger;
 
+let chains = [];
+let resids = [];
+let restypes = [];
+let atomNames = [];
+
 const timestep = 1 / 60;
+
+const atomRadius = 0.01;
+const scale = .03;
+const translation = new THREE.Vector3(-0.8, 0.3, -.5);
 
 const tmpVector1 = new THREE.Vector3();
 const tmpVector2 = new THREE.Vector3();
 const tmpQuatertnion = new THREE.Quaternion();
 var dummy = new THREE.Object3D();
+const carbonMaterial = new THREE.MeshPhongMaterial({
+  color: 0x555555,
+  flatShading: true,
+});
+const oxygenMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  flatShading: true,
+});
+const nitrogenMaterial = new THREE.MeshPhongMaterial({
+  color: 0x0000ff,
+  flatShading: true,
+});
+const sulfurMaterial = new THREE.MeshPhongMaterial({
+  color: 0xfdc12a,
+  flatShading: true,
+});
+
+const atomGeometry = new THREE.SphereGeometry(atomRadius, 32, 32);
+const sphereShape = new CANNON.Sphere(atomRadius);
 
 let controls;
-let box1, box2, connector;
 
 let grabbing = false;
 
 const spheres = [];
-const sphereRadius = 0.05;
-
 const bodies = [];
 const meshes = [];
-let grabbedMesh, grabbedBody;
+let grabbedMesh;
 
 init();
 animate();
@@ -40,7 +66,7 @@ function init() {
   document.body.appendChild(container);
 
   world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, -10, 0), // m/s²
+    gravity: new CANNON.Vec3(0, 0, 0), // m/s²
   });
 
   scene = new THREE.Scene();
@@ -137,61 +163,7 @@ function init() {
   hand2.add(handModelFactory.createHandModel(hand2, "mesh"));
   scene.add(hand2);
 
-  // Dummy boxes
-  const geometry = new THREE.BoxGeometry(
-    sphereRadius,
-    sphereRadius,
-    sphereRadius
-  );
-  const material = new THREE.MeshStandardMaterial({
-    color: 0xff0000,
-    roughness: 1.0,
-    metalness: 0.0,
-  });
-
-  const halfExtents = new CANNON.Vec3(
-    sphereRadius / 2,
-    sphereRadius / 2,
-    sphereRadius / 2
-  );
-
-  const boxShape = new CANNON.Box(halfExtents);
-  const boxShape2 = new CANNON.Box(halfExtents.scale(3));
-
-  box1 = new THREE.Mesh(geometry, material);
-  box1.geometry.computeBoundingSphere();
-  box1.position.set(-0.2, 1.4, -0.5);
-  box1.castShadow = true;
-  const box1Body = new CANNON.Body({ mass: 0, shape: boxShape });
-  box1Body.position.set(-0.2, 1.4, -0.5);
-  world.addBody(box1Body);
-
-  box2 = new THREE.Mesh(geometry, material);
-  box2.geometry.computeBoundingSphere();
-  box2.position.set(-0.2, 1, -0.5);
-  box2.scale.multiplyScalar(3);
-  box2.castShadow = true;
-  const box2Body = new CANNON.Body({ mass: 20, shape: boxShape2 });
-  box2Body.position.set(-0.2, 1, -0.5);
-  world.addBody(box2Body);
-
-  const sphereGeometry = new THREE.SphereBufferGeometry(0.01, 32, 16);
-  const sphereMaterial = new THREE.MeshLambertMaterial({ color: "yellow" });
-  connector = new THREE.InstancedMesh(sphereGeometry, sphereMaterial, 10);
-  connector.instanceMatrix.setUsage(THREE.DynamicDrawUsage); // will be updated every frame
-  scene.add(connector);
-
-  spheres.push(box1, box2);
-  // grabbedMesh = box1;
-  meshes.push(box1, box2);
-  // meshes.push(box2);
-  bodies.push(box1Body, box2Body);
-  grabbedBody = box1Body;
-  bodies.push(box2Body);
-  scene.add(box1, box2);
-
-  const distConstraint = new CANNON.DistanceConstraint(box1Body, box2Body);
-  world.addConstraint(distConstraint);
+  buildMolecule(pdb);
 
   window.addEventListener("resize", onWindowResize);
 }
@@ -268,25 +240,52 @@ function updateMeshPositions() {
       meshes[i].quaternion.copy(bodies[i].quaternion);
     }
   }
+}
 
-  // grabbedMesh.getWorldPosition(tmpVector1);
-  // grabbedMesh.getWorldQuaternion(tmpQuatertnion);
+function buildMolecule(pdb) {
+  const mass = 1;
 
-  // grabbedBody.position.copy(tmpVector1);
-  // grabbedBody.quaternion.copy(tmpQuatertnion);
+  const lineas = pdb.split("\n");
+  let atomMaterial;
 
-  for (let index = 1; index <= 10; index++) {
-    var p0 = new THREE.Vector3();
-    var p1 = new THREE.Vector3();
-    var pf = new THREE.Vector3();
+  for (let i = 0; i < lineas.length; i++) {
+    if (lineas[i].substring(0, 4) == "ATOM") {
+      resids.push(parseInt(lineas[i].substring(23, 26)));
+      restypes.push(lineas[i].substring(17, 20).trim());
+      atomNames.push(lineas[i].substring(13, 15).trim());
 
-    p0.setFromMatrixPosition(box1.matrixWorld);
-    p1.setFromMatrixPosition(box2.matrixWorld);
-    pf.lerpVectors(p0, p1, index / 10);
+      if (lineas[i].substring(77, 78).trim() === "C") {
+        atomMaterial = carbonMaterial;
+      }
+      if (lineas[i].substring(77, 78).trim() === "N") {
+        atomMaterial = nitrogenMaterial;
+      }
+      if (lineas[i].substring(77, 78).trim() === "O") {
+        atomMaterial = oxygenMaterial;
+      }
+      if (lineas[i].substring(77, 78).trim() === "S") {
+        atomMaterial = sulfurMaterial;
+      }
 
-    dummy.position.copy(pf);
-    dummy.updateMatrix();
-    connector.setMatrixAt(index, dummy.matrix);
+      const atom = new THREE.Mesh(atomGeometry, atomMaterial);
+      atom.castShadow = true;
+      atom.position.set(
+        parseFloat(lineas[i].substring(30, 38)),
+        parseFloat(lineas[i].substring(38, 46)),
+        parseFloat(lineas[i].substring(46, 54))
+      );
+      atom.position.multiplyScalar(scale);
+      atom.position.add(translation);
+      meshes.push(atom);
+
+      const sphereBody = new CANNON.Body({
+        mass: mass,
+        shape: sphereShape,
+      });
+      sphereBody.position.copy(atom.position);
+      bodies.push(sphereBody);
+      world.addBody(sphereBody);
+      scene.add(atom);
+    }
   }
-  connector.instanceMatrix.needsUpdate = true;
 }
